15a60dfb930e11491f365a6ad5fe496e7c439432
Avalanche <albecker@hotmail.de>
2025-10-08 17:10:55 +0200

    PressAnyKey now leads to Main Menu, When any key is pressed

--- Source/UITestProject/Private/Widgets/Widget_ActivatableBase.cpp
+++ Source/UITestProject/Private/Widgets/Widget_ActivatableBase.cpp
@@ -2,4 +2,13 @@
 
 
 #include "Widgets/Widget_ActivatableBase.h"
+#include "Controllers/MyFrontendController.h"
 
+AMyFrontendController* UWidget_ActivatableBase::GetOwningFrontendController()
+{
+	if (!CachedOwningFrontendController.IsValid())
+	{
+		CachedOwningFrontendController = GetOwningPlayer<AMyFrontendController>();
+	}
+	return CachedOwningFrontendController.IsValid() ? CachedOwningFrontendController.Get() : nullptr;
+}
